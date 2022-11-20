f805e90c0ed2ed12b17ec77bf8a23407d510a6ac
LeonardoNatale <leonardo@multis.co>
2022-11-20 19:10:26 +0100

    add profile page

---
+++ queries/get-profile.ts
@@ -0,0 +1,19 @@
+import { gql } from '@apollo/client'
+
+export const getProfile = gql`
+  query Profile($handle: Handle!) {
+    profile(request: { handle: $handle }) {
+      id
+      name
+      bio
+      picture {
+        ... on MediaSet {
+          original {
+            url
+          }
+        }
+      }
+      handle
+    }
+  }
+`
