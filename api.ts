9278383f310604450791e2731bff6bd80ac599b8
Matias Rietig <matias.rietig@corp.meraki.com>
2022-11-23 19:39:25 +0100

    simple wallet connection logic

--- api.ts
+++ api.ts
@@ -1,4 +1,4 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
 
 const API_URL = 'https://api-mumbai.lens.dev'
 
@@ -7,3 +7,27 @@ export const client = new ApolloClient({
   uri: API_URL,
   cache: new InMemoryCache()
 })
+
+export const challenge = gql`
+  query Challenge($address: EthereumAddress!) {
+    challenge(request: { address: $address }) {
+      text
+    }
+  }
+`
+
+export const authenticate = gql`
+  mutation Authenticate(
+    $address: EthereumAddress!
+    $signature: Signature!
+  ) {
+    authenticate(request: {
+      address: $address,
+      signature: $signature
+    }) {
+      accessToken
+      refreshToken
+    }
+  }
+`
+
