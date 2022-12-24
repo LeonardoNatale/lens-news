e7b4cd01d11c96504ace44d7fbbb55fba900ac94
LeonardoNatale <natale.leonardo1997@gmail.com>
2022-12-24 15:11:19 +0100

    improve feed layout

--- src/pages/article/[id].tsx
+++ src/pages/article/[id].tsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@apollo/client'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import ReactMarkdown from 'react-markdown'
 import { GET_ARTICLE } from '../../modules/article/get-article'
 
 const Article = () => {
@@ -26,21 +27,24 @@ const Article = () => {
   const article = data.publication
 
   return (
-    <div className="flex flex-col justify-center items-center">
-      <Link href={`/profile/${article.profile.handle}`}>
-        <p className="cursor-pointer text-violet-600 text-lg font-medium text-center mt-2 mb-2">
-          {`Author: ${article.profile.handle}`}
+    <div className="flex justify-center">
+      <div className="w-2/3">
+        <Link href={`/profile/${article.profile.handle}`}>
+          <p className="cursor-pointer text-violet-600 text-lg font-medium text-center mt-2 mb-2">
+            {`Author: ${article.profile.handle}`}
+          </p>
+        </Link>
+        <h1 className="text-5xl mb-6 font-bold">{article.metadata.name}</h1>
+        {/* <img className="w-48" src={profile.avatarUrl || 'https://picsum.photos/200'} /> */}
+        <p className="text-xl text-center mt-6">{article.metadata.description}</p>
+        <p className="mt-4 text-gray-700 leading-relaxed font-serif">
+          <ReactMarkdown>{article.metadata.content}</ReactMarkdown>
         </p>
-      </Link>
-      <h1 className="text-5xl mb-6 font-bold">{article.metadata.name}</h1>
-      {/* <img className="w-48" src={profile.avatarUrl || 'https://picsum.photos/200'} /> */}
-      <p className="text-xl text-center mt-6">{article.metadata.description}</p>
-      <p className="text-base text-gray-400  text-center mt-2">
-        {article.metadata.content}
-      </p>
-      {/* <p className="text-pink-600 text-sm font-medium text-center">
+
+        {/* <p className="text-pink-600 text-sm font-medium text-center">
         {`TAGS: ${article.metadata.tags.join(',')}`}
       </p> */}
+      </div>
     </div>
   )
 }
